666f13ce0b0023d1f374507b56709f23172771a6
David Torres <davidj_torres@hotmail.com>
2025-03-11 08:24:26 -0500

    updating repository interface

--- so-good-contacts/src/repositories/interfaces.ts
+++ so-good-contacts/src/repositories/interfaces.ts
@@ -1,8 +1,9 @@
-// path/to/repository/repositoryInterface.ts
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface IRepository<T> {
-    create(entity: T): Promise<T>;
-    findById(id: string): Promise<T | null>;
-    findAll(): Promise<T[]>;
-    update(id: string, entity: T): Promise<T | null>;
-    delete(id: string): Promise<boolean>;
-}
+    create(data: T): Promise<T>;
+    findAll(query?: Record<string, any>): Promise<T[]>;
+    findOne(query: Record<string, any>): Promise<T | null>;
+    update(query: Record<string, any>, data: Partial<T>): Promise<number>;
+    delete(query: Record<string, any>): Promise<number>;
+  }
+  
