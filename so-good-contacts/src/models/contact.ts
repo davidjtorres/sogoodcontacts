ef1a63853534ccb0ebdc941847bceb9610625843
David Torres <davidj_torres@hotmail.com>
2025-03-11 11:33:36 -0500

    updating contact model

--- so-good-contacts/src/models/contact.ts
+++ so-good-contacts/src/models/contact.ts
@@ -1,6 +1,16 @@
 export interface Contact {
-    id?: string;
-    name: string;
-    email: string;
-    phone: string;
+	id?: string;
+    user_id: string;
+	first_name: string;
+	last_name: string;
+	phone_number?: string;
+	email: string;
+	address?: {
+		address_line_1: string;
+		address_line_2?: string;
+		city: string;
+		state: string;
+		zipcode: string;
+		country: string;
+	};
 }
