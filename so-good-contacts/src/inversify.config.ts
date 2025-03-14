f4165b77e628d87692b523532229d3c5813dd664
David Torres <davidj_torres@hotmail.com>
2025-03-14 11:25:27 -0500

    adding more services to container

--- so-good-contacts/src/inversify.config.ts
+++ so-good-contacts/src/inversify.config.ts
@@ -3,14 +3,28 @@ import { Container } from "inversify";
 import { Db } from "mongodb";
 import { connectMongoDB } from "@/database/mongo-db-driver";
 import { ContactRepository } from "@/repositories/contact-repository";
+import { ConstantContactApi } from "@/constant-contact-gateway/constant-contact-api";
+import { ConstantContactApiAdapter } from "@/constant-contact-gateway/constant-contact-api-adapter";
+import axios from "axios";
+import { User } from "./models/user";
+import { ContactsService } from "./services/contacts-service";
 
-const container = new Container();
+export const getContainer = async (user: User) => {
+	const container = new Container();
 
-// Bind MongoDB instance
-const dbInstance = await connectMongoDB();
-container.bind<Db>("Database").toConstantValue(dbInstance);
+	const dbInstance = await connectMongoDB();
+	container.bind<Db>("Database").toConstantValue(dbInstance);
 
-// Bind repositories
-container.bind<ContactRepository>(ContactRepository).toSelf();
+	container.bind<ContactRepository>(ContactRepository).toSelf();
 
-export { container };
+	container.bind<ConstantContactApi>(ConstantContactApi).toDynamicValue(() => {
+		const axiosInstance = axios.create({
+			baseURL: process.env.CONSTANT_CONTACT_API_URL,
+		});
+		return new ConstantContactApi(axiosInstance, user.constant_contact_token || "");
+	});
+	container.bind<ConstantContactApiAdapter>(ConstantContactApiAdapter).toSelf();
+	container.bind<ContactsService>(ContactsService).toSelf();
+
+	return container;
+};
